31f5bf83041449b24763d272503f82c4d243b554
Trevor Puccini <trevorpuccini21@gmail.com>
2020-04-09 11:20:34 -0700

    Create VideoPlayer container using redux connect

--- src/containers/VideoPlayerContainer.js
+++ src/containers/VideoPlayerContainer.js
@@ -1,7 +1,13 @@
 import { connect } from 'react-redux';
 import VideoPlayer from '../components/VideoPlayer.js';
 
-var VideoPlayerContainer = () => {};
+let mapStateToProps = (state) => {
+  return {
+    currentVideo: state.currentVideo
+  };
+}
+
+var VideoPlayerContainer = connect(mapStateToProps, null)(VideoPlayer);
 
 //TODO: define a VideoPlayerContainer component which will hook up your action
 //dispatchers with your VideoPlayer component props.
--- src/store/store.js
+++ src/store/store.js
@@ -3,15 +3,11 @@ import thunk from 'redux-thunk';
 import rootReducer from './../reducers/main.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 
-var initialState = {
-  videoList: exampleVideoData,
-  currentVideo: exampleVideoData[0]
-};
+
 
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
 var reduxStore = createStore(
       rootReducer,
-      initialState,
       applyMiddleware(thunk)
 );
 
