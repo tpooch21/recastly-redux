723edadc5023ff67d5518d8162b28420f77f0d86
Trevor Puccini <trevorpuccini21@gmail.com>
2020-04-09 15:47:12 -0700

    Refactor App to initialize component containers

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import VideoList from './VideoList.js';
-import VideoPlayer from './VideoPlayer.js';
-import Search from './Search.js';
+// import VideoList from './VideoList.js';
+// import VideoPlayer from './VideoPlayer.js';
+// import Search from './Search.js';
+import SearchContainer from '../containers/SearchContainer.js';
+import VideoListContainer from '../containers/VideoListContainer.js';
+import VideoPlayerContainer from '../containers/VideoPlayerContainer.js';
 
 //  sprint
 // import {connect} from 'react-redux';
@@ -48,18 +51,15 @@ class App extends React.Component {
       <div>
         <nav className="navbar">
           <div className="col-md-6 col-md-offset-3">
-            <Search getYouTubeVideos={this.getYouTubeVideos}/>
+            <SearchContainer />
           </div>
         </nav>
         <div className="row">
           <div className="col-md-7">
-            <VideoPlayer video={this.state.currentVideo}/>
+            <VideoPlayerContainer />
           </div>
           <div className="col-md-5">
-            <VideoList
-              handleVideoListEntryTitleClick={this.handleVideoListEntryTitleClick.bind(this)}
-              videos={this.state.videos}
-            />
+            <VideoListContainer />
           </div>
         </div>
       </div>
--- src/store/store.js
+++ src/store/store.js
@@ -3,12 +3,18 @@ import thunk from 'redux-thunk';
 import rootReducer from './../reducers/main.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 
+var initialState = {
+  currentVideo: exampleVideoData[0],
+  videoList: exampleVideoData
+};
+
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
 var reduxStore = createStore(
-      rootReducer,
-      exampleVideoData,
-      applyMiddleware(thunk)
+  rootReducer,
+  initialState,
+  applyMiddleware(thunk)
 );
 
 
 export default reduxStore;
+
