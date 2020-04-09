e092803606d0ecd8bcc9129e6e2b47bc00cf29fd
Trevor Puccini <trevorpuccini21@gmail.com>
2020-04-09 11:30:16 -0700

    Add VideoList container using redux connect

--- src/containers/VideoListContainer.js
+++ src/containers/VideoListContainer.js
@@ -2,6 +2,20 @@ import { connect } from 'react-redux';
 import VideoList from './../components/VideoList.js';
 import changeVideo from './../actions/currentVideo.js';
 
+let mapStateToProps = state => {
+  return {
+    videos : state.videos
+  };
+};
+
+let mapDispatchToProps = dispatch => {
+  return {
+    handleVideoListEntryClick: (video) => dispatch(changeVideo(video))
+  };
+};
+
+
+
 var VideoListContainer = connect(mapStateToProps, mapDispatchToProps)(VideoList);
 
 //TODO: define a VideoListContainer component which will hook up your action
